51b54e8a084dc418ffc1ad294e1f9c8d67407078
MiRacLe.RPZ <miracle@rpz.name>
2026-07-03 22:23:11 +0300

    Pass t.me links directly to Telegram.app

--- open-uri-in-native-app.bash
+++ open-uri-in-native-app.bash
@@ -37,25 +37,8 @@ fi
 if [[ $URI =~ ^https://t\.me/ ]]; then
   TELEGRAM=$(xdg-mime query default x-scheme-handler/tg)
   if [[ -n $TELEGRAM ]]; then
-    # private invite: https://t.me/+<invite> or https://t.me/joinchat/<invite>
-    if [[ $URI =~ ^https://t\.me/\+([A-Za-z0-9_-]+)$ ]]; then
-      NEW="tg://join?invite=${BASH_REMATCH[1]}"
-    elif [[ $URI =~ ^https://t\.me/joinchat/([A-Za-z0-9_-]+)$ ]]; then
-      NEW="tg://join?invite=${BASH_REMATCH[1]}"
-    # forum topic: path order is domain/thread/post, but tg:// wants post before thread
-    elif [[ $URI =~ ^https://t\.me/([A-Za-z0-9_]{5,32})/([0-9]+)/([0-9]+)/?$ ]]; then
-      NEW="tg://resolve?domain=${BASH_REMATCH[1]}&post=${BASH_REMATCH[3]}&thread=${BASH_REMATCH[2]}"
-    elif [[ $URI =~ ^https://t\.me/([A-Za-z0-9_]{5,32})/([0-9]+)/?$ ]]; then
-      NEW="tg://resolve?domain=${BASH_REMATCH[1]}&post=${BASH_REMATCH[2]}"
-    elif [[ $URI =~ ^https://t\.me/([A-Za-z0-9_]{5,32})\?start=(.+)$ ]]; then
-      NEW="tg://resolve?domain=${BASH_REMATCH[1]}&start=${BASH_REMATCH[2]}"
-    elif [[ $URI =~ ^https://t\.me/([A-Za-z0-9_]{5,32})/?$ ]]; then
-      NEW="tg://resolve?domain=${BASH_REMATCH[1]}"
-    fi
-    if [[ -n ${NEW:-} ]]; then
-      gtk-launch $TELEGRAM $NEW
-      exit 0;
-    fi
+    gtk-launch $TELEGRAM $URI
+    exit 0;
   fi
 fi
 
